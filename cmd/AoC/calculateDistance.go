7e49f65958e4e9fcaaae79945e04e67e8d724b0c
Kyle Hinton <kylehinton3@gmail.com>
2024-12-09 09:57:19 +0000

    Added day 1 - puzzle 2

--- cmd/AoC/calculateDistance.go
+++ cmd/AoC/calculateDistance.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"log"
 	"slices"
 )
 
-func calculateDistance(listA, listB []int) {
+func calculateDistance(listA, listB []int) int {
 	var distanceSlice []int
+	totalDistance := 0
 
 	slices.Sort(listA)
 	slices.Sort(listB)
-
-	totalDistance := 0
 
 	for i := 0; i < len(listA); i++ {
 		distance := listB[i] - listA[i]
@@ -24,8 +22,44 @@ func calculateDistance(listA, listB []int) {
 		} else {
 			totalDistance = totalDistance - distance
 		}
-
 	}
 
-	log.Print(totalDistance)
+	return totalDistance
+}
+
+func calculateScore(listA, listB []int) int {
+	var similarityScore []int
+	totalScore := 0
+	similarity := make(map[int]int)
+
+	slices.Sort(listA)
+	slices.Sort(listB)
+
+	for i := 0; i < len(listA); i++ {
+		matches := 0
+		for _, item := range listB {
+			if listA[i] == item {
+				matches++
+			}
+		}
+
+		if matches > 0 {
+			similarity[listA[i]] = matches
+		}
+	}
+
+	for k, v := range similarity {
+		score := k * v
+		similarityScore = append(similarityScore, score)
+	}
+
+	for _, score := range similarityScore {
+		if score > 0 {
+			totalScore = totalScore + score
+		} else {
+			totalScore = totalScore - score
+		}
+	}
+
+	return totalScore
 }
--- cmd/AoC/main.go
+++ cmd/AoC/main.go
@@ -1,9 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	filePath := "list.txt"
 
 	fileContent := readFile(filePath)
 	listA, listB := splitSlices(fileContent)
-	calculateDistance(listA, listB)
+	totalDistance := calculateDistance(listA, listB)
+	totalScore := calculateScore(listA, listB)
+
+	fmt.Printf("Distance: %d\n", totalDistance)
+	fmt.Printf("Similarity Score: %d\n", totalScore)
 }
